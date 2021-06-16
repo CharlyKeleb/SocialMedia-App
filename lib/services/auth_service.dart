08c99817348737d89bf79fe9ef6af354fa98eff1
Deyi Kong <deyikong@yahoo.com>
2021-06-15 20:56:44 -0500

    added login function

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:social_media_app/services/user_service.dart';
 import 'package:social_media_app/utils/firebase.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthService {
   User getCurrentUser() {
@@ -40,16 +46,63 @@ class AuthService {
   }
 
   Future<bool> loginUser({String email, String password}) async {
-    var res = await firebaseAuth.signInWithEmailAndPassword(
-      email: '$email',
-      password: '$password',
-    );
+    // use http to login user to 99huaren's back end service.
 
-    if (res.user != null) {
+    final response = await http.post(
+        Uri.parse('http://api.99huaren.local:3001/api/authorizations'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'email': '$email',
+          'password': '$password'
+        }),
+
+    );
+    if (response.statusCode == 201) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+      final authResponse = AuthResponse.fromJson(jsonDecode(response.body));
+
+// Create storage
+      final storage = new FlutterSecureStorage();
+
+// // Read value
+//       String value = await storage.read(key: key);
+//
+// // Read all values
+//       Map<String, String> allValues = await storage.readAll();
+//
+// // Delete value
+//       await storage.delete(key: key);
+//
+// // Delete all
+//       await storage.deleteAll();
+
+// Write value
+      await storage.write(key: 'access_token', value: authResponse.accessToken);
+
+      UserService userService = UserService();
+      userService.getCurrentUser();
+      // save the token to the device
       return true;
     } else {
-      return false;
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      throw Exception('Failed to create album.');
     }
+    //log
+    return true;
+    // var res = await firebaseAuth.signInWithEmailAndPassword(
+    //   email: '$email',
+    //   password: '$password',
+    // );
+    //
+    // if (res.user != null) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
   }
 
   forgotPassword(String email) async {
@@ -82,5 +135,21 @@ class AuthService {
     } else {
       return e;
     }
+  }
+}
+
+class AuthResponse {
+  final String accessToken;
+  final String expiresIn;
+  final String tokenType;
+
+  AuthResponse({this.accessToken, this.expiresIn, this.tokenType});
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return AuthResponse(
+      accessToken: json['access_token'],
+      expiresIn: json['expires_in'],
+      tokenType: json['token_type'],
+    );
   }
 }
--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,14 +1,37 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:social_media_app/models/user.dart';
 import 'package:social_media_app/services/services.dart';
 import 'package:social_media_app/utils/firebase.dart';
+import 'package:http/http.dart' as http;
 
 class UserService extends Service {
   String currentUid() {
     return firebaseAuth.currentUser.uid;
   }
+
+  Future<LoggedUser> getCurrentUser() async {
+    final storage = new FlutterSecureStorage();
+    final accessToken = await storage.read(key: 'access_token');
+    final response = await http.get(
+      Uri.parse('http://api.99huaren.local:3001/api/user'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ' + accessToken
+      },
+    );
+    if (response.statusCode == 200) {
+      storage.write(key: 'logged_in_user', value: response.body);
+    }else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      throw Exception('Failed to create album.');
+    }
+
+    }
 
   setUserStatus(bool isOnline) {
     var user = firebaseAuth.currentUser;
@@ -37,5 +60,37 @@ class UserService extends Service {
     });
 
     return true;
+  }
+}
+/*
+avatar: "http://www.hellowaresolutions.com/Content/img/logo.png"
+created_at: "2021-04-03 03:19:52"
+email: "info@hellowaresolutions.com"
+id: 1
+introduction: "Libero voluptatibus repellat eos non."
+message_count: 0
+name: "Summer"
+notification_count: 0
+pin_count: 0
+public_email: true
+settings: "{\"replyReverse\":true}"
+wechat_connected: false
+ */
+class LoggedUser {
+  final int id;
+  final String avatar;
+  final String createdAt;
+  final String email;
+  // final String introduction;
+
+  LoggedUser({this.id, this.avatar, this.createdAt, this.email});
+
+  factory LoggedUser.fromJson(Map<String, dynamic> json) {
+    return LoggedUser(
+      id: json['id'],
+      avatar: json['avatar'],
+      createdAt: json['created_at'],
+      email: json['email'],
+    );
   }
 }
